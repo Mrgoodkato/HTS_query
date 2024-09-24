ad57f0cc7a5250c0613d3328022dea1ff686314b
Mrgoodkato <81311715+Mrgoodkato@users.noreply.github.com>
2024-09-23 21:12:59 -0500

    Working on the Flask app

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask
-from main import *
-import pymongo
+import sys, os
+from flask import Flask, render_template
+sys.path.append(os.path.abspath('main'))
+from main import query_hts_db, query_string_db
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world!'
-
+    
+    return render_template('home.html.j2', title='Hello there')
 
 if __name__ == '__main__':
     app.run(debug=True)
