1ffbb15ebc93e03b8454ffa27535a939d23c5d9a
Mrgoodkato <mrgoodkato@gmail.com>
2024-10-18 18:58:25 -0500

    Some changes and additions to the validation in textAreaInput.js

--- static/js/textAreaInput.js
+++ static/js/textAreaInput.js
@@ -1,14 +1,38 @@
 const txtArea = document.getElementById('hts_txt_area');
+const warning = document.getElementById('warning_input');
+const modalErrors = document.getElementById('warning_modal');
+const errorList = document.getElementById('error_list');
+
+const validCharacters = /^[0-9.\n]*$/g;
+const linePattern = /(?:^[\d]{4}(?:\.[\d]{2}){1,3}$)|(?:^[\d]{4,10})/gm;
+let isPasting =false;
+
+function validateInput(value){
+
+    return validCharacters.test(value)
+
+}
 
 txtArea.addEventListener('input', ()=>{
 
-    let input = txtArea.value;
-
-    const validCharacters = /^[0-9.\n]*$/g;
-
-    if(!validCharacters.test(input)){
+    if(!validateInput(txtArea.value)){
+        let input = txtArea.value
         txtArea.value = input.replace(/[^0-9.\n]/g, '')
-        console.log('Replaced' )
+        warning.style.display = 'block';
     }
+    else warning.style.display = 'none';
+
+})
+
+txtArea.addEventListener('paste', ()=>{
+
+    isPasting = true;
+
+    if(!validateInput(txtArea.value)){
+        console.log('modal')
+        modalErrors.style.display = 'flex';
+    }
+    
+    isPasting = false;
 
 })
