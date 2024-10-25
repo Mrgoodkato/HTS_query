38629036e851e1cccdfa159016539ad06b6dadb5
Mrgoodkato <mrgoodkato@gmail.com>
2024-10-24 20:51:55 -0500

    More work on textAreaInput.js

--- static/js/textAreaInput.js
+++ static/js/textAreaInput.js
@@ -2,12 +2,45 @@ const txtArea = document.getElementById('hts_txt_area');
 const warning = document.getElementById('warning_input');
 const errorArea = document.getElementById('error_container');
 const errorList = document.getElementById('error_list');
-const closeErrors = document.getElementById('close_errors');
 
 const htsPattern = /(?:^[\d]{4}\.[\d]{2}\.[\d]{2}\.[\d]{2}$|\\r)|(?:^[\d]{4}\.[\d]{2}\.(?:[\d]{2}|[\d]{4})$|\\r)|(?:^[\d]{4}\.(?:[\d]{2}|[\d]{4}|[\d]{6})$)|(?:^[\d]{4}$|\\r)|(?:^[\d]{6}$|\\r)|(?:^[\d]{8}$|\\r)|(?:^[\d]{10}$|\\r)/;
 const validCharacters = /^[0-9.\n]*$/g;
 const linePattern = /(?:^[\d]{4}(?:\.[\d]{2}){1,3}$)|(?:^[\d]{4,10})/gm;
 let isPasting =false;
+
+function grabTextAreaInput(value){
+
+    const textArray = value.split('\n');
+    const parsedText = {
+        valid: [],
+        errors: []
+    };
+    textArray.forEach(text => {
+        
+        if(!text.match(htsPattern)) parsedText.errors.push(text);
+        else parsedText.valid.push(text);
+    });
+    return parsedText;
+
+}
+
+function populateErrorArea(errors){
+    
+    errors.forEach(error => {
+        const paragraph = document.createElement('p');
+        paragraph.textContent = error;
+        errorList.append(paragraph);
+    })
+}
+
+function populateTextArea(text){
+
+    text.forEach(txt =>{
+        console.log(txt)
+        txtArea.value += txt + '\n';
+    })
+
+}
 
 txtArea.addEventListener('input', ()=>{
     let isValid = !!txtArea.value.match(validCharacters);
@@ -22,25 +55,14 @@ txtArea.addEventListener('input', ()=>{
 
 txtArea.addEventListener('keydown', (event)=>{
     if(event.key == 'Enter'){
-        const errors = grabTextAreaInput(txtArea.value);
+        const parsedText = grabTextAreaInput(txtArea.value);
         
-        if(errors){
+        if(parsedText.errors){
             event.preventDefault();
-            errors.forEach(error => {
-                errorList.textContent += error + '\n'
-            })
+            errorList.replaceChildren();
+            populateErrorArea(parsedText.errors);
+            populateTextArea(parsedText.valid);
         }
-        
-        textArray.forEach(text => {
-            if(!text.match(htsPattern)){
-                event.preventDefault();
-                warning.style.display = 'block';
-            }
-            else{
-                warning.style.display = 'none';
-            }
-        });
-
     }
 
 })
@@ -48,10 +70,14 @@ txtArea.addEventListener('keydown', (event)=>{
 txtArea.addEventListener('paste', (event)=>{
     
     isPasting = true;
-    let isValid = !!event.clipboardData.getData('text/plain').match(validCharacters);
-    if(!isValid && isPasting){
-        errorArea.style.display = 'flex';
-        errorList.textContent = event.clipboardData.getData('text');
+    const isValid = !!event.clipboardData.getData('text/plain').match(validCharacters);
+    const parsedText = grabTextAreaInput(event.clipboardData.getData('text'));
+    if(!isValid && isPasting && parsedText.errors){
+        console.log(parsedText)
+        event.preventDefault();
+        errorList.replaceChildren();
+        populateErrorArea(parsedText.errors);
+        populateTextArea(parsedText.valid);
     }
 
     isPasting = false;
@@ -59,20 +85,3 @@ txtArea.addEventListener('paste', (event)=>{
 
 })
 
-closeErrors.addEventListener('click', ()=>{
-
-    errorList.textContent = '';
-    errorArea.style.display = 'none';
-
-})
-
-function grabTextAreaInput(value){
-
-    const textArray = value.split('\n');
-    const invalidText = [];
-    textArray.forEach(text => {
-        if(!text.match(htsPattern)) invalidText.push(text);
-    });
-    return invalidText;
-
-}
