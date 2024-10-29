bcd49d6e24a0ba7473fe68c5f923219cb6be34cf
Mrgoodkato <81311715+Mrgoodkato@users.noreply.github.com>
2024-10-28 22:04:14 -0500

    Work on user input in textAreaInput.js and home page

--- static/js/textAreaInput.js
+++ static/js/textAreaInput.js
@@ -1,20 +1,66 @@
 const txtArea = document.getElementById('hts_txt_area');
+const manualArea = document.querySelector('[data-htsinput]');
+console.log(manualArea)
 const warning = document.getElementById('warning_input');
 const errorArea = document.getElementById('error_container');
 const errorList = document.getElementById('error_list');
+const punctuationErrorList = document.getElementById('punctuation_error_list');
+const allowedKeys = [
+    "Backspace",
+    "Tab",
+    "Escape",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "Delete",
+    "Insert",
+    "Home",
+    "End",
+    "PageUp",
+    "PageDown",
+    "Shift",
+    "Control",
+    "Alt",
+    "CapsLock",
+    "NumLock",
+    "ScrollLock",
+    "F1",
+    "F2",
+    "F3",
+    "F4",
+    "F5",
+    "F6",
+    "F7",
+    "F8",
+    "F9",
+    "F10",
+    "F11",
+    "F12"
+  ];;
 
 const htsPattern = /(?:^[\d]{4}\.[\d]{2}\.[\d]{2}\.[\d]{2}$|\\r)|(?:^[\d]{4}\.[\d]{2}\.(?:[\d]{2}|[\d]{4})$|\\r)|(?:^[\d]{4}\.(?:[\d]{2}|[\d]{4}|[\d]{6})$)|(?:^[\d]{4}$|\\r)|(?:^[\d]{6}$|\\r)|(?:^[\d]{8}$|\\r)|(?:^[\d]{10}$|\\r)/;
 const validCharacters = /^[0-9.\n]*$/g;
 const linePattern = /(?:^[\d]{4}(?:\.[\d]{2}){1,3}$)|(?:^[\d]{4,10})/gm;
-let isPasting =false;
+const punctuationPattern = /[ \r!\"#$%&\'()*+,-./:;<=>?@\[\]\^_`{|}~—]/g;
+const patternManualInput = /[a-zA-Z \r!\"#$%&\'()*+,\-:;<=>?@\[\]\^_`{|}~—\\]/g;
 
+//TEXT AREA LOGIC
 function grabTextAreaInput(value){
 
-    const textArray = value.split('\n');
+    const punctuationErrors = value.match(punctuationPattern);
+    const cleanedText = value.replace(punctuationPattern, '');
+    const textArray = cleanedText.split('\n');
     const parsedText = {
         valid: [],
-        errors: []
+        errors: [],
+        invalidPunctuation: null
     };
+    if(punctuationErrors){
+        const errSet = new Set(punctuationErrors);
+        if(errSet.has('.')) errSet.delete('.');
+        parsedText.invalidPunctuation =([...errSet].join(' '));
+    }
     textArray.forEach(text => {
         
         if(!text.match(htsPattern)) parsedText.errors.push(text);
@@ -36,52 +82,53 @@ function populateErrorArea(errors){
 function populateTextArea(text){
 
     text.forEach(txt =>{
-        console.log(txt)
         txtArea.value += txt + '\n';
     })
 
 }
 
-txtArea.addEventListener('input', ()=>{
-    let isValid = !!txtArea.value.match(validCharacters);
-    if(!isValid && !isPasting){
-        let input = txtArea.value;
-        txtArea.value = input.replace(/[^0-9.\n]/g, '');
-        warning.style.display = 'block';
-    }
-    else warning.style.display = 'none';
+function populatePunctuationErrors(text){
+    const paragraph = document.createElement('p');
+    paragraph.textContent =text;
+    punctuationErrorList.replaceChildren(paragraph);
+}
 
-})
-
-txtArea.addEventListener('keydown', (event)=>{
-    if(event.key == 'Enter'){
-        const parsedText = grabTextAreaInput(txtArea.value);
-        
-        if(parsedText.errors){
-            event.preventDefault();
-            errorList.replaceChildren();
-            populateErrorArea(parsedText.errors);
-            populateTextArea(parsedText.valid);
-        }
-    }
+txtArea.addEventListener('input', (event)=>{
+    
+    event.preventDefault();
 
 })
 
 txtArea.addEventListener('paste', (event)=>{
-    
-    isPasting = true;
     const isValid = !!event.clipboardData.getData('text/plain').match(validCharacters);
     const parsedText = grabTextAreaInput(event.clipboardData.getData('text'));
-    if(!isValid && isPasting && parsedText.errors){
-        console.log(parsedText)
+    if(!isValid && parsedText.errors){
         event.preventDefault();
+        if(parsedText.invalidPunctuation) {
+            populatePunctuationErrors(parsedText.invalidPunctuation);           
+        }
         errorList.replaceChildren();
         populateErrorArea(parsedText.errors);
         populateTextArea(parsedText.valid);
     }
+})
 
-    isPasting = false;
+//INPUT AREA LOGIC
 
-
+manualArea.addEventListener('keydown', (event)=>{
+    //Prevents backtick behavior causing problems
+    if (event.key === "`") {
+        event.preventDefault();
+    }
+    //Prevents special function keys, and copy paste as well as auto fill to trigger error
+    if(allowedKeys.includes(event.key) || event.ctrlKey || event.key == undefined) {
+        warning.style.display = 'none';
+        return;
+    }
+    //Prevents letters and wrong characters input in the field
+    if(event.key.match(patternManualInput)){
+        event.preventDefault();
+        warning.style.display = 'block';
+    }else warning.style.display = 'none';
 })
 
