b428ea22d40f7ee1a4b733743cf2727b21357bcc
Mrgoodkato <81311715+Mrgoodkato@users.noreply.github.com>
2024-11-26 19:32:59 -0500

    FE Work

--- static/js/options.js
+++ static/js/options.js
@@ -1,13 +1,21 @@
 const optionBtns = document.querySelectorAll('[data-option]');
 const optionContainers = document.querySelectorAll('[data-optionContainer');
 
+
+function toggleDisplayBtn(container){
+
+    if(container.style.display == 'block') container.style.display = 'none';
+    else container.style.display = 'block';
+
+}
+
 function toggleOptions(selectedBtn){
 
     optionBtns.forEach(button => {
         if(button.getAttribute('data-option') == selectedBtn){
             optionContainers.forEach(container => {
                 if(container.getAttribute('data-optionContainer') == selectedBtn){
-                    container.style.display = 'block';
+                    toggleDisplayBtn(container);
                 }else{
                     container.style.display = 'none';
                 }
