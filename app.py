af7e394ac159a2eb57ebe6325e4bdb8cf21ac846
Mrgoodkato <mrgoodkato@gmail.com>
2024-11-30 16:36:23 -0500

    Backend work for validation of form submission, will use form instead of JS fetch

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sys, os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 sys.path.append(os.path.abspath('main'))
 from main import query_hts_db, query_string_db
 from main.utils.util_functions import processTextAreaInput
@@ -12,12 +12,22 @@ def index():
 
 @app.route('/process_query', methods=['POST'])
 def process_query():
-    query = processTextAreaInput(request.args['user_input'])
-    errors = query['errors']
-    query_result = query_hts_db.queryHTSNumber(query['query_list'], testing=True)
-    if not query_result: return 'No data'
-    return render_template('query_results.html.j2', query_result=query_result, errors=errors)
 
+    try:
+        data = request.get_json()
+        user_input = data.get('user_input') if data else None
+        if not user_input:
+            return jsonify({"message": "No input provided"}), 400
+        processed_data = user_input
+        query = processTextAreaInput(processed_data)
+        errors = query['errors']
+        query_result = query_hts_db.queryHTSNumber(query['query_list'], testing=True)
+        if not query_result: return 'No data'
+        return render_template('query_results.html.j2', query_result=query_result, errors=errors)
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+   
 @app.route('/process_file_query')
 def process_file_query():
     
--- static/js/submit.js
+++ static/js/submit.js
@@ -1,21 +1,15 @@
 import config from "./config.js";
 const submitPaste = document.getElementById('submit_paste');
-const textArea = document.get
+const textArea = document.getElementById('hts_txt_area');
 const submitManuel = document.getElementById('submit_manual');
 const submitFile = document.getElementById('submit_file');
 
 submitPaste.addEventListener('click', () => {
     
-    const textArea = document.getElementById('hts_txt_area');
     const endpointURL = `${config.API_BASE_URL}/process_query`;
-
     fetch(endpointURL, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({user_input: textArea.value})
     })
-    .then(response => response.json())
-    .then(result => console.log('Success!', result))
-    .catch(error => console.log('Error', error));
-
 });
