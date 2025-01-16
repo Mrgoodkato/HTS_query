5d740d9484e2b95d71ce34a0082f98d06b6d52e9
Ricardo <mrgoodkato@gmail.com>
2025-01-15 20:01:14 -0500

    minor changes to ignore logs

--- logs/basic_logs.py
+++
@@ -1,8 +0,0 @@
-def basic_logger(data: list[dict,any], filename: str):
-    """Method to log different txt results to the folder 'logs/txt_logs/'
-
-    Args:
-        data (list[dict,any]): Data to log
-        filename (str): Filename to be used
-    """
-    with open(f'logs/txt_logs/{filename}.txt', 'w', encoding="utf-8") as log_file: log_file.write(str(data))
